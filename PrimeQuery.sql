b48a74828e917478cd2d6cf2fb9d74eb8216bbd6
Pedra96 <52459440+Pedra96@users.noreply.github.com>
2022-12-16 14:03:30 +0100

    Add files via upload
    esercizio completato

---
+++ PrimeQuery.sql
@@ -0,0 +1,13 @@
+﻿Select * FROM teachers
+Select head_of_department FROM departments
+Select * FROM degrees WHERE level = 'magistrale'
+Select * FROM students WHERE name LIKE 'marco'
+Select * FROM courses WHERE cfu > 12
+Select * FROM courses WHERE cfu NOT BETWEEN 5 AND 10
+Select * FROM courses WHERE period='I semestre' and year=1
+Select * FROM courses WHERE website IS NULL
+Select * FROM teachers WHERE phone IS NOT NULL
+--Bonus
+Select * FROM students WHERE name LIKE 'e%'
+Select COUNT(*) AS TeachersNameThatStartsWithE FROM teachers WHERE name like 'e%'
+Select COUNT(*) AS NULLPhone FROM teachers WHERE phone IS NULL
